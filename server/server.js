131e0a14aae75a3aad3dd3f92531e57274340d50
Manav Bokinala <manav.bokinala@gmail.com>
2017-08-11 12:13:24 -0700

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -15,18 +16,36 @@ app.post('/todos', (req, res) => {
   });
 
   todo.save().then((doc) => {
-    res.status(201).send(doc);
+    res.send(doc);
   }, (e) => {
     res.status(400).send(e);
   });
 });
 
-app.get('/todos', (request, response) => {
-	Todo.find().then((todos) => {
-		response.send({todos});
-	}, (err) => {
-		response.status(400).send(err);
-	});
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(3000, () => {
